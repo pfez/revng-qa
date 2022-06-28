46a84138025de28ca6f6baa4f7d6a0cc0c9dd6cb
Pietro Fezzardi <pietro@rev.ng>
2022-06-28 18:05:31 +0200

    Add stack test

---
+++ share/revng/test/tests/analysis/Decompilation/dynamic_native/stack.c
@@ -0,0 +1,22 @@
+struct struct_t {
+  long long w;
+  int x;
+  char y;
+  char z;
+};
+
+extern void init_struct(struct struct_t *s);
+
+int stack_struct() {
+  struct struct_t s;
+  init_struct(&s);
+  return s.w + s.x + s.w + s.z;
+}
+
+int stack() {
+  return 0;
+}
+
+int main() {
+  return stack_struct() + stack();
+}
